6252c0e487d0357529f605b1f5a90a1437d15748
sandymohamed <sandymohamede2@gmail.com>
2024-09-17 16:05:07 +0300

    Create mini-forms folder & CalendarSettingsForm Upd AddTeacherForm & AddTeacherSchema

--- src/components/mini-forms/calendarSettingsForm.tsx
+++ src/components/mini-forms/calendarSettingsForm.tsx
@@ -1,49 +1,72 @@
-// import { FieldValues, Path, UseFormRegister } from "react-hook-form";
+import { FieldValues, Path, UseFormRegister } from "react-hook-form";
 
-// import { CheckboxGroup, Heading, RadioField, Row } from "@/components";
+import {
+  // CheckboxGroup,
+  Heading,
+  InputField,
+  RadioField,
+  Row,
+} from "@/components";
 
-// export interface TCalendarSettingsFormProps<T extends FieldValues> {
-//   register: UseFormRegister<T>;
-//   name: Path<T>;
-// }
+export interface TCalendarSettingsFormProps<T extends FieldValues> {
+  register: UseFormRegister<T>;
+  calendar_setting: Path<T>;
+  calendar_color_by: Path<T>;
+  calendar_color: Path<T>;
+  errors: any;
+}
 
-// const CalendarSettingsForm = <T extends FieldValues>({
-//   register,
-//   name,
-// }: TCalendarSettingsFormProps<T>) => {
-//   return (
-//     <div>
-//       <Heading text="التقويم" />
+const CalendarSettingsForm = <T extends FieldValues>({
+  register,
+  calendar_setting,
+  calendar_color_by,
+  calendar_color,
+  errors,
+}: TCalendarSettingsFormProps<T>) => {
+  return (
+    <div>
+      <Heading text="التقويم" />
+      <Row>
+        <RadioField
+          name={calendar_setting}
+          label="إعدادات التقويم"
+          options={[
+            { label: "شهر", value: "Month" },
+            { label: "اسبوع", value: "Week" },
+            { label: "يوم", value: "Day" },
+          ]}
+          register={register}
+          error={errors.calendar_setting?.message}
+          isRequired={true}
+        />
+        <RadioField
+          name={calendar_color_by}
+          label="درس التقويم اللون حسب "
+          options={[
+            { label: "طالب", value: "Student" },
+            { label: "موقع", value: "Website" },
+            { label: "درس", value: "Lesson" },
+          ]}
+          register={register}
+          error={errors.calendar_color_by?.message}
+          isRequired={true}
+        />
+      </Row>
+      <Row>
+        <InputField
+          type="color"
+          label="لون التقويم"
+          placeholder="#1C8A44 "
+          register={register}
+          name={calendar_color}
+          error={errors.calendar_color?.message as string}
+        />
+        <article className="group"></article>
+      </Row>
 
-//       <RadioField
-//         name={name}
-//         label="Select your gender"
-//         options={[
-//           { label: "Male", value: "male" },
-//           { label: "Female", value: "female" },
-//           { label: "Other", value: "other" },
-//         ]}
-//         register={register}
-//         error={errors.gender?.message}
-//         isRequired={true}
-//       />
+      <hr className="hr" />
+    </div>
+  );
+};
 
-//       <Row>
-//         <CheckboxGroup
-//           register={register}
-//           name={name}
-//           options={[
-//             { label: "Option 1", value: "option1" },
-//             { label: "Option 2", value: "option2" },
-//           ]}
-//           isRequired={true}
-//         />
-//         <article className="group"></article>
-//       </Row>
-
-//       <hr className="hr" />
-//     </div>
-//   );
-// };
-
-// export default CalendarSettingsForm;
+export default CalendarSettingsForm;
