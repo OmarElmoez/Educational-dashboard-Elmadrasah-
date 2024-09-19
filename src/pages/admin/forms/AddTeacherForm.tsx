ee457d8448ef838b2f50264144e54c5847f46bc3
OmarElmoez <omarelmoez5@gmail.com>
2024-09-19 16:13:34 +0300

    modify CalendarSettingsForm structure

--- src/pages/admin/forms/AddTeacherForm.tsx
+++ src/pages/admin/forms/AddTeacherForm.tsx
@@ -577,9 +577,6 @@ const AddTeacherForm = () => {
 
       <CalendarSettingsForm
         register={register}
-        calendar_setting="calendar_setting"
-        calendar_color="calendar_color"
-        calendar_color_by="calendar_color_by"
         errors={errors}
         setValue={setValue}
       />
