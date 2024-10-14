0bdce1a44d45639779767fc183f7eac4e5273a8a
OmarElmoez <omarelmoez5@gmail.com>
2024-10-14 12:13:00 +0300

    fix edit invoice issues

--- src/types/User.ts
+++ src/types/User.ts
@@ -1,22 +1,21 @@
 export type TUser = {
-    id: number;
-    first_name: string;
-    last_name: string;
-    email: string;
-    phone: string;
-    image: string;
-    gender: string;
-    birth_date: string;
-    date_joined: string;
-    message: string;
-    status: number;
-    new_notification: number;
+  id: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+  phone: string;
+  image: string;
+  gender: string;
+  birth_date: string;
+  date_joined: string;
+  message: string;
+  status: number;
+  new_notification: number;
 };
 
-
 export type TUserStatistics = {
-    total_lessons: number;
-    total_attended: number;
-    total_scheduled: number;
-    total_missed: number;
+  total_lessons: number;
+  total_attended: number;
+  total_scheduled: number;
+  total_missed: number;
 };
