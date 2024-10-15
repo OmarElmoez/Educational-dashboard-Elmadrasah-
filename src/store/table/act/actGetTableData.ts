e805fb1789c8fd9aa5865c4496b5ddd198832498
Sandy <sandymohamede2@gmail.com>
2024-10-15 11:14:22 +0300

    upd PackageBalanceList

--- src/store/table/act/actGetTableData.ts
+++ src/store/table/act/actGetTableData.ts
@@ -18,9 +18,20 @@ const actGetTableData = <T>() =>
 
       try {
         if (searchTerm) {
-          url += `?start_date=${searchTerm.start_date || ""}&end_date=${
-            searchTerm.end_date || ""  // Fixed: was startDate, changed to endDate
-          }&is_active=${searchTerm.is_active || ""}&name=${searchTerm.name}`;
+          url += '?';
+          if(searchTerm.start_date) url+=`start_date=${searchTerm.start_date }`;
+          if(searchTerm.end_date) url+=`&end_date=${searchTerm.end_date }`;
+          if(searchTerm.is_active) url+=`&is_active=${searchTerm.is_active }`;
+          if(searchTerm.name) url+=`&name=${searchTerm.name }`;
+
+          // url += `?${ searchTerm.start_date ? 'start_date=' searchTerm.start_date : ''}&end_date=${
+          //   searchTerm.end_date || ""  // Fixed: was startDate, changed to endDate
+          // }&is_active=${searchTerm.is_active || ""}&name=${searchTerm.name}`;
+         
+         
+          // url += `?start_date=${searchTerm.start_date || ""}&end_date=${
+          //   searchTerm.end_date || ""  // Fixed: was startDate, changed to endDate
+          // }&is_active=${searchTerm.is_active || ""}&name=${searchTerm.name}`;
 
           url += `&page=${page}`;
         } else {
