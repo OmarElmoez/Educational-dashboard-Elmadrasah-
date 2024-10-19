2ca6ceec09e8c49ed47ce43a8c7e2f9e7d69b21c
OmarElmoez <omarelmoez5@gmail.com>
2024-10-19 13:41:02 +0300

    fix confilct with dropdownWithSearch

--- src/components/forms/drop-down-with-search/DropdownWithSearch.tsx
+++ src/components/forms/drop-down-with-search/DropdownWithSearch.tsx
@@ -4,6 +4,7 @@ import { TOption } from "@/types/Dropdown";
 import { useDebounce } from "@/hooks";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
 import { actGetDropdownOptions } from "@/store/single-actions";
+import { useLocation } from "react-router-dom";
 
 const { select_box, popup_box, search_box, options_box } = styles;
 
@@ -33,11 +34,15 @@ const DropdownWithSearch = ({
   const dispatch = useAppDispatch();
   const { user } = useAppSelector((state) => state.auth);
 
+  const location = useLocation();
+  const isBalanceRoute = location.pathname === "/admin/balance-list";
+  
+
   const handleOptionClick = (option: TOption) => {
     setSelectedOption(option);
     setIsOpen(false);
 
-    if(setSelectedCustomer) {
+    if (setSelectedCustomer) {
       setSelectedCustomer(option);
     }
 
@@ -71,16 +76,12 @@ const DropdownWithSearch = ({
         onClick={() => setIsOpen(!isOpen)}
       >
         {/* here will be the selected option */}
-        {selectedOption && selectedCustomer ? (
-          <span>{selectedOption.label}</span>
+
+        {(isBalanceRoute ? (selectedCustomer) : selectedOption) ? (
+          <span>{selectedOption?.label}</span>
         ) : (
-          <span
-            className="dropdown-placeholder"
-          >
-            {placeholder}
-          </span>
+          <span className="dropdown-placeholder">{placeholder}</span>
         )}
-        
       </section>
       {isOpen && (
         <section className={popup_box}>
