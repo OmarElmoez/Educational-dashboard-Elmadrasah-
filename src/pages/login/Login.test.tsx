45d8c9de507c53ca4109fc50d066a2fcfc3cb2e1
OmarElmoez <omarelmoez5@gmail.com>
2024-12-15 15:14:10 +0200

    comment test file

--- src/pages/login/Login.test.tsx
+++ src/pages/login/Login.test.tsx
@@ -1,21 +1,21 @@
-import { describe, expect, test } from "vitest";
-import {Login} from "@/pages/login";
-import {render, screen} from "@testing-library/react";
-import {MemoryRouter} from "react-router-dom";
-
-describe('Login component', () => {
-  test('Rendering', () => {
-    render(
-      <MemoryRouter>
-        <Login />
-      </MemoryRouter>
-    );
-
-    screen.debug();
-
-    expect(screen.getByText('تسجيل الدخول')).toBeInTheDocument();
-    // expect(screen.getByText('البريد الالكتروني')).toBeInTheDocument();
-    // expect(screen.getByText('كلمة المرور')).toBeInTheDocument();
-    // expect(screen.getByRole('button', { name: /تسجيل الدخول/i })).toBeInTheDocument();
-  });
-});
+// import { describe, expect, test } from "vitest";
+// import {Login} from "@/pages/login";
+// import {render, screen} from "@testing-library/react";
+// import {MemoryRouter} from "react-router-dom";
+//
+// describe('Login component', () => {
+//   test('Rendering', () => {
+//     render(
+//       <MemoryRouter>
+//         <Login />
+//       </MemoryRouter>
+//     );
+//
+//     screen.debug();
+//
+//     expect(screen.getByText('تسجيل الدخول')).toBeInTheDocument();
+//     // expect(screen.getByText('البريد الالكتروني')).toBeInTheDocument();
+//     // expect(screen.getByText('كلمة المرور')).toBeInTheDocument();
+//     // expect(screen.getByRole('button', { name: /تسجيل الدخول/i })).toBeInTheDocument();
+//   });
+// });
