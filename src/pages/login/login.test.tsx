d0f4888635812763b11c98c11b4baa6c64b02ed2
OmarElmoez <omarelmoez5@gmail.com>
2025-02-03 16:25:17 +0200

    comment login.test.tsx file

--- src/pages/login/login.test.tsx
+++ src/pages/login/login.test.tsx
@@ -1,35 +1,21 @@
-// import {  it, expect, vi } from 'vitest';
-// import { render, screen } from '@testing-library/react';
-// import {Login} from "@/pages/login";
-// import {JSX} from "react";
-// import {createMockStore} from "@/tests/__utils__/testUtils.ts";
-// import {Provider} from "react-redux";
-// import {PersistGate} from "redux-persist/integration/react";
-// import {BrowserRouter} from "react-router-dom";
+// import { render, screen, fireEvent } from '@testing-library/react';
+// import { Provider } from 'react-redux';
+// import { configureStore } from '@reduxjs/toolkit';
+// import { describe, beforeEach } from "vitest";
+// import auth from "@/store/auth/authSlice";
 //
-// const TestWrapper = ({children}: { children: JSX.Element }): JSX.Element => {
+// describe('SignIn form', () => {
 //
-//   const {store, persistor} = createMockStore()
+//   let store;
 //
-//   console.log('from test wrapper: ', children);
+//   beforeEach(() => {
+//     // Create fresh store instance for each test
+//     store = configureStore({
+//       reducer: {
+//         auth,
+//       },
+//     });
+//   });
 //
-//   return (
-//     <Provider store={store}>
-//       <PersistGate loading={null} persistor={persistor}>
-//         <BrowserRouter>
-//           {children}
-//         </BrowserRouter>
-//       </PersistGate>
-//     </Provider>
-//   );
-// };
 //
-// it('renders login form', async () => {
-//   render(<TestWrapper><Login /></TestWrapper>);
-//
-//   screen.debug()
-//
-//   // expect(screen.findByText('البريد الالكتروني', { selector: 'span' })).toBeInTheDocument();
-//   // expect(screen.getByText(/تسجيل الدخول/i)).toBeInTheDocument();
-//   // expect(screen.findByRole('button', { name: /تسجيل الدخول/i })).toBeInTheDocument();
-// });
+// })
