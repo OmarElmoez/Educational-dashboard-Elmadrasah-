138da809fa8ba21ff55192de750479cc56094dd3
OmarElmoez <omarelmoez5@gmail.com>
2025-04-23 16:16:04 +0200

    O-fix import for filter form for orders table

--- src/pages/admin/orders/list/ordersList.tsx
+++ src/pages/admin/orders/list/ordersList.tsx
@@ -10,7 +10,7 @@ import {
 import { Box, Button } from "@mui/material";
 import Drawer from "@mui/material/Drawer";
 import { useState } from "react";
-import FilterForm from "@/pages/admin/orders/FilterForm.tsx";
+import FilterForm from "../FilterForm.tsx";
 
 export type TOrdersFilterData = {
   start_date: string;
